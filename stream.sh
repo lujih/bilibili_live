196ba06837bff33fd39d3b5b565f1c81ebc32c2d
褚山真寻 <88123458+lujih@users.noreply.github.com>
2024-11-18 21:06:53 +0800

    Update stream.sh
    优化

--- stream.sh
+++ stream.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# 推流地址
+STREAM_URL="__STREAM_URL__"
+
+# 推流逻辑
 while true; do
     for video in /bilibili_live/videos/*; do
         echo "正在推流视频文件：$video"
-        ffmpeg -re -i "$video" -vcodec libx264 -preset veryfast -tune zerolatency -acodec aac -threads 2 -f flv "rtmp://live-push.bilivideo.com/live-bvc/?streamname=live_485962382_77000177&key=e02b8345e0d39a254a540758f9a4b7a9&schedule=rtmp&pflag=1"
+        ffmpeg -re -i "$video" -vcodec libx264 -preset veryfast -tune zerolatency -acodec aac -threads 2 -f flv "$STREAM_URL"
     done
     echo "所有视频推流完成，5 秒后重新循环..."
     sleep 5
